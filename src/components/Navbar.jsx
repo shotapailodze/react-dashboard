520b93ae607d477d0ae0a3677912e4e60403d9e7
shotapailodze <shotikophailodze1998@gmail.com>
2023-11-29 13:37:35 +0400

    left side done

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,19 @@
+import { TextInput } from "@tremor/react";
+import { SearchIcon } from "@heroicons/react/solid";
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div
+      id="top"
+      className="relative w-full sm:flex justify-between items-center p-2 "
+    >
+      <h1 className="font-bold text-gray-300">Dashboard</h1>
+      <div className="py-2">
+        <TextInput icon={SearchIcon} placeholder="Search..." />
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
